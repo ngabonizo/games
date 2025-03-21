2a1720e96e9542a6d5bd809faf075bf958da5c59
NGABO <ngaboizo2000@gmail.com>
2025-03-21 12:39:06 +0200

    creating game logic with nDimensional arrays

---
+++ TicTacToe Game/tictactoe.js
@@ -0,0 +1,9 @@
+const cells = document.querySelectorAll('.cell');
+
+const statusText = document.querySelector('#statusText');
+
+const restart = document.querySelector('#restartButton');
+
+const winConditions = [
+  [],
+]
