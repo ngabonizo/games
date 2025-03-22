a94bff7f614d810128410f454976a02072982dc3
NGABO <ngaboizo2000@gmail.com>
2025-03-22 15:57:48 +0200

    creating game functions

--- TicTacToe Game/tictactoe.js
+++ TicTacToe Game/tictactoe.js
@@ -5,5 +5,40 @@ const statusText = document.querySelector('#statusText');
 const restart = document.querySelector('#restartButton');
 
 const winConditions = [
-  [],
-]
+  [0,1,2],
+   [3,4,5],
+   [6,7,8],
+   [0,3,6],
+   [1,4,7],
+   [2,5,8],
+   [0,4,8],
+   [2,4,6]
+];
+
+let options = ['','','','','','','','',''];
+let currentPlayer = 'x';
+let running = false;
+
+function initializeGame() {
+
+};
+
+function cellClicked(){
+
+};
+
+function updateCell(cell, index){
+
+}
+
+function changePlayer(){
+
+};
+
+function checkWinner(){
+
+};
+
+function restartGame(){
+  
+}
