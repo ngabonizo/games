ea39bb152c611c627d5e0c5cb38bd6f346c75552
NGABO <ngaboizo2000@gmail.com>
2025-03-23 15:35:01 +0200

    game logic complete - But needs debbuging because game is not working

--- TicTacToe Game/tictactoe.js
+++ TicTacToe Game/tictactoe.js
@@ -20,25 +20,66 @@ let currentPlayer = 'x';
 let running = false;
 
 function initializeGame() {
-
+ cells.forEach(cell => cell.addEventListener("click",cellClicked));
+ restartButton.addEventListener("click",restartGame);
+ statusText.textContent = `${currentPlayer}'s turn`;
+  running = true;
 };
 
 function cellClicked(){
+  const cellIndex = this.getAttribute("cellIndex");
 
+  if(options[cellIndex] !== '' || !running){
+    return;
+  }
+  updateCell(this, cellIndex);
+  checkWinner();
 };
 
 function updateCell(cell, index){
-
+options[index] = currentPlayer;
+cell.textContent = currentPlayer;
 }
 
 function changePlayer(){
-
+  currentPlayer = (currentPlayer == 'x') ? 'o': 'x';
+  statusText.textContent = `${currentPlayer}'s turn`;
 };
 
 function checkWinner(){
+ let roundWon = false;
+  
+ for(let i=0; i<winConditions.length; i++){
+  const condition = winConditions[i];
+  const cellA = options[condition[0]]
+  const cellB = options[condition[1]]
+  const cellC = options[condition[2]]
+  
 
+  if(cellA == ''|| cellB == ''|| cellC == ''){
+    continue;
+  }
+if(cellA == cellB && cellB == cellC){
+  roundWon = true;
+  break;
+}
+ }
+
+ if(roundWon){
+  statusText.textContent = `${currentPlayer} wins!`;
+  running = false;
+ }else if(!options.includes('')){
+  statusText.textContent = `Draw!`;
+  running = false;
+ }else{
+  changePlayer();
+ }
 };
 
 function restartGame(){
-  
+  currentPlayer = 'x';
+  let options = ['','','','','','','','',''];
+  statusText.textContent = `${currentPlayer}'s turn`;
+  cells.forEach(cell => cell.textContent= '');
+  running = true;
 }
